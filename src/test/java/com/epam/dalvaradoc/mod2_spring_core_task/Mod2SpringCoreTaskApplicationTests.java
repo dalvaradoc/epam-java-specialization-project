95fa07dbeb02a97f3a4f16ca0a16fde2eba2bee6
alejandro <dalvaradoc@unal.edu.co>
2025-03-20 22:45:45 -0500

    first commit

---
+++ src/test/java/com/epam/dalvaradoc/mod2_spring_core_task/Mod2SpringCoreTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.dalvaradoc.mod2_spring_core_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mod2SpringCoreTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
