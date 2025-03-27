18b6a5f01e0a6022076ee1588fe5c157204bab9e
alejandro <dalvaradoc@unal.edu.co>
2025-03-27 18:00:25 -0500

    Trainees reading with json file

--- src/main/java/com/epam/dalvaradoc/mod2_spring_core_task/configs/StorageBeansConfig.java
+++ src/main/java/com/epam/dalvaradoc/mod2_spring_core_task/configs/StorageBeansConfig.java
@@ -1,19 +1,19 @@
 package com.epam.dalvaradoc.mod2_spring_core_task.configs;
 
 import java.io.FileInputStream;
-import java.io.IOException;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
-import java.util.Properties;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.epam.dalvaradoc.mod2_spring_core_task.dao.Trainee;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.core.json.JsonReadFeature;
+import com.epam.dalvaradoc.mod2_spring_core_task.dao.User;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 
@@ -27,32 +27,28 @@ public class StorageBeansConfig {
 
   @Bean
   public Map<String, Trainee> traineesMap(){
-    Map<String, Trainee> result = getData(traineesMapFile, Trainee.class);
+    Map<String, Trainee> result = getData(traineesMapFile, Trainee.class, new TypeReference<List<Trainee>>() {});
     // result.forEach((v,k) -> LOGGER.info(v + " " + k));
     return result;
   }
 
-  public <V> Map<String,V> getData(String dataFileRoute, Class<V> valueClass) {
+  public <V> Map<String,V> getData(String dataFileRoute, Class<V> valueClass, TypeReference<List<V>> typeReference) {
     Map<String, V> values = new TreeMap<>();
-    Properties valuesData = new Properties();
     try (FileInputStream input = new FileInputStream(dataFileRoute)) {
-      ObjectMapper mapper = JsonMapper.builder().enable(JsonReadFeature.ALLOW_BACKSLASH_ESCAPING_ANY_CHARACTER).build();
-      valuesData.load(input);      
-      valuesData.forEach((key, value) -> {
-        String userId = (String) key;
-        try {
-          V valueObject = mapper.readValue((String) value, valueClass);
-          // LOGGER.info(((V) valueObject).toString());
-          values.put(userId, valueObject);
-        } catch (JsonProcessingException e) {
-          LOGGER.error(e.getMessage());
-        }
-      });
+      ObjectMapper mapper = JsonMapper.builder().build();
+      List<V> listOfObjects = mapper.readValue(input, typeReference);
+      // LOGGER.info(listOfObjects.toString());
+      if (User.class.isAssignableFrom(valueClass)){
+        values = listOfObjects.stream().collect(Collectors.toMap(o -> ((User)o).getUserId(), o -> o));
+        LOGGER.info(listOfObjects.get(0).getClass().getName());
+      }
       LOGGER.info("Finished loading mock data for " + valueClass.getSimpleName() + " map");
       return values;
-    } catch (IOException e) {
+    } catch (Exception e) {
       e.printStackTrace();
+      LOGGER.error("WTF?????");
+      LOGGER.error(e.getMessage());
     }
-    return new HashMap<>();
+    return new TreeMap<>();
   }
 }
--- src/test/java/com/epam/dalvaradoc/mod2_spring_core_task/Mod2SpringCoreTaskApplicationTests.java
+++ src/test/java/com/epam/dalvaradoc/mod2_spring_core_task/Mod2SpringCoreTaskApplicationTests.java
@@ -1,13 +1,44 @@
 package com.epam.dalvaradoc.mod2_spring_core_task;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.epam.dalvaradoc.mod2_spring_core_task.dao.Trainee;
+import com.epam.dalvaradoc.mod2_spring_core_task.services.TraineeService;
+
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootTest
+@Slf4j
 class Mod2SpringCoreTaskApplicationTests {
+	@Autowired
+	Map<String, Trainee> traineesMap;
+	@Autowired
+	private TraineeService traineeService;
 
 	@Test
 	void contextLoads() {
+		assertNotNull(traineeService);
+		assertNotNull(traineesMap);
 	}
 
+	@Test
+	public void traineesMapInitializedTest() {
+		assertNotNull(traineesMap.get("1"));
+	}
+
+	@Test
+	public void getTraineeByIdTest() {
+		Trainee trainee = traineesMap.get("1");
+		assertEquals(trainee, traineeService.getTraineeById("1"));
+		assertNull(traineesMap.get("300"));
+	}
 }
