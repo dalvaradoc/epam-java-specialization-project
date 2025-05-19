8f14b03407ae1b6b54ff7d5ec59ed55fbba5fee6
alejandro <dalvaradoc@unal.edu.co>
2025-05-19 02:41:10 -0500

    check credentials return 404 when not found username

--- src/main/java/com/epam/dalvaradoc/mod2_spring_core_task/aop/CheckCredentialsAspect.java
+++ src/main/java/com/epam/dalvaradoc/mod2_spring_core_task/aop/CheckCredentialsAspect.java
@@ -6,7 +6,9 @@ import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.epam.dalvaradoc.mod2_spring_core_task.dao.User;
 import com.epam.dalvaradoc.mod2_spring_core_task.dto.AuthenticationDTO;
@@ -29,37 +31,34 @@ public class CheckCredentialsAspect {
   @Around("@annotation(CheckCredentials)")
   public Object checkCredentials(ProceedingJoinPoint joinPoint) throws Throwable {
     Object[] args = joinPoint.getArgs();
-    if (args.length == 0 && !checkIfUsernameAndPasswordPassed(args) && !checkIfUserPassed(args) && !checkIfAuthenticationDTOPassed(args)) {
-        LOGGER.error("Invalid method arguments for @CheckCredentials");
-        throw new IllegalArgumentException("Invalid method arguments for @CheckCredentials");
+    if (args.length == 0 && !checkIfUsernameAndPasswordPassed(args) && !checkIfUserPassed(args)
+        && !checkIfAuthenticationDTOPassed(args)) {
+      LOGGER.error("Invalid method arguments for @CheckCredentials");
+      throw new IllegalArgumentException("Invalid method arguments for @CheckCredentials");
     }
+
+    String username;
+    String password;
 
     if (checkIfUserPassed(args)) {
-      User user = (User) args[args.length-1];
-      String username = user.getUsername();
-      String password = user.getPassword();
-      if (Optional.ofNullable(userRepository.findByUsername(username)).filter(u -> u.getPassword().equals(password)).isEmpty()) {
-        LOGGER.error("Invalid credentials for user: " + username);
-        throw new BadCredentialsException("Invalid credentials");
-      }
-      return joinPoint.proceed();
+      User user = (User) args[args.length - 1];
+      username = user.getUsername();
+      password = user.getPassword();
+    } else if (checkIfAuthenticationDTOPassed(args)) {
+      AuthenticationDTO authenticationDTO = (AuthenticationDTO) args[args.length - 1];
+      username = authenticationDTO.getUsername();
+      password = authenticationDTO.getPassword();
+    } else {
+      username = (String) args[args.length - 2];
+      password = (String) args[args.length - 1];
     }
-
-    if (checkIfAuthenticationDTOPassed(args)){
-      AuthenticationDTO authenticationDTO = (AuthenticationDTO) args[args.length-1];
-      String username = authenticationDTO.getUsername();
-      String password = authenticationDTO.getPassword();
-      if (Optional.ofNullable(userRepository.findByUsername(username)).filter(u -> u.getPassword().equals(password)).isEmpty()) {
-        LOGGER.error("Invalid credentials for user: " + username);
-        throw new BadCredentialsException("Invalid credentials");
-      }
-      return joinPoint.proceed();
-    }
-
-    String username = (String) args[args.length-2];
-    String password = (String) args[args.length-1];
 
     User user = userRepository.findByUsername(username);
+    if (user == null) {
+      LOGGER.error("User not found: " + username);
+      throw new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found");
+    }
+
     if (Optional.ofNullable(user).filter(u -> u.getPassword().equals(password)).isEmpty()) {
       LOGGER.error("Invalid credentials for user: " + username);
       throw new BadCredentialsException("Invalid credentials");
@@ -68,14 +67,14 @@ public class CheckCredentialsAspect {
   }
 
   private boolean checkIfUsernameAndPasswordPassed(Object[] args) {
-    return args.length > 1 && args[args.length-1] instanceof String && args[args.length-2] instanceof String;
+    return args.length > 1 && args[args.length - 1] instanceof String && args[args.length - 2] instanceof String;
   }
 
-  private boolean checkIfUserPassed (Object[] args) {
-    return User.class.isAssignableFrom(args[args.length-1].getClass());
+  private boolean checkIfUserPassed(Object[] args) {
+    return User.class.isAssignableFrom(args[args.length - 1].getClass());
   }
 
-  private boolean checkIfAuthenticationDTOPassed (Object[] args) {
-    return AuthenticationDTO.class.isAssignableFrom(args[args.length-1].getClass());
+  private boolean checkIfAuthenticationDTOPassed(Object[] args) {
+    return AuthenticationDTO.class.isAssignableFrom(args[args.length - 1].getClass());
   }
 }
