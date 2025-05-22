c3e2275dcf6c1e3860e6d9ca52a640c1ee54afdc
alejandro <dalvaradoc@unal.edu.co>
2025-05-22 17:11:45 -0500

    TraineeController with swagger docs

--- src/main/java/com/epam/dalvaradoc/mod2_spring_core_task/dto/TrainingTypeDTO.java
+++ src/main/java/com/epam/dalvaradoc/mod2_spring_core_task/dto/TrainingTypeDTO.java
@@ -1,10 +1,13 @@
 package com.epam.dalvaradoc.mod2_spring_core_task.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class TrainingTypeDTO {
   private Long id;
   private String name;
